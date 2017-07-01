177ae3559ce5afee12b49a23ad8eb951b970a1d4
Ali Shahbour <shahbour@gmail.com>
2017-07-01 11:07:40 +0300

    use spring security OAuth2FeignRequestInterceptor instead of creating my own version.

--- task/src/main/java/com/shahbour/config/OAuth2FeignAutoConfiguration.java
+++ task/src/main/java/com/shahbour/config/OAuth2FeignAutoConfiguration.java
@@ -1,24 +1,12 @@
 package com.shahbour.config;
 
-import com.shahbour.domain.Customer;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
-
-import java.util.List;
 
 /**
  * Created by shahbour on 14/06/17.
@@ -28,11 +16,9 @@ import java.util.List;
 @Slf4j
 public class OAuth2FeignAutoConfiguration {
 
-
     @Bean
     public OAuth2FeignRequestInterceptor oAuth2FeignRequestInterceptor(OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
-        OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(details, oauth2ClientContext);
-        return new OAuth2FeignRequestInterceptor(oAuth2RestTemplate);
+        return new OAuth2FeignRequestInterceptor(oauth2ClientContext,details);
     }
 
 
